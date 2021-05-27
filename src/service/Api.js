dbe25029a95318eea9eec8df117204aa62e83010
radja iqbal <sanusiiqbal36@gmail.com>
2021-05-27 16:25:59 +0700

    Deploy Update

--- src/service/Api.js
+++ src/service/Api.js
@@ -4,129 +4,129 @@ export default class Api {
     // Daftar pegawai
     // Mengambil Seluruh Data
     getPegawai() {
-        return axios.get(`https://natunasandmine.com/api/daftarPegawai`)
+        return axios.get(`https://server.natunasandmine.com/api/daftarPegawai`)
     }
     // Menambah Data
     addPegawai(value) {
-        return axios.post(`https://natunasandmine.com/api/daftarPegawai`, value)
+        return axios.post(`https://server.natunasandmine.com/api/daftarPegawai`, value)
     }
     // Mengubah Data
     editPegawai(params, value) {
-        return axios.put(`https://natunasandmine.com/api/daftarPegawai/${params}`, value)
+        return axios.put(`https://server.natunasandmine.com/api/daftarPegawai/${params}`, value)
     }
     //Menghapus Data
     deletePegawai(params) {
-        return axios.delete(`https://natunasandmine.com/api/daftarPegawai/${params}`)
+        return axios.delete(`https://server.natunasandmine.com/api/daftarPegawai/${params}`)
     }
     
     //KASIR
     // Mengambil Seluruh Data
     getKasir() {
-        return axios.get(`https://natunasandmine.com/api/daftarKasir`)
+        return axios.get(`https://server.natunasandmine.com/api/daftarKasir`)
     }
     // Menambah Data
     addKasir(value) {
-        return axios.post(`https://natunasandmine.com/api/daftarKasir`, value)
+        return axios.post(`https://server.natunasandmine.com/api/daftarKasir`, value)
     }
     // Mengubah Data
     editKasir(params, value) {
         console.log(`${params}:${value}`)
-        return axios.put(`https://natunasandmine.com/api/daftarKasir/${params}`, value)
+        return axios.put(`https://server.natunasandmine.com/api/daftarKasir/${params}`, value)
     }
     //Menghapus Data
     deleteKasir(params) {
-        return axios.delete(`https://natunasandmine.com/api/daftarKasir/${params}`)
+        return axios.delete(`https://server.natunasandmine.com/api/daftarKasir/${params}`)
     }
 
     //Dompet
     getDompetKasir() {
-        return axios.get('https://natunasandmine.com/api/dompetKasir')
+        return axios.get('https://server.natunasandmine.com/api/dompetKasir')
     }
     getDompetPegawai() {
-        return axios.get('https://natunasandmine.com/api/dompetPegawai')
+        return axios.get('https://server.natunasandmine.com/api/dompetPegawai')
     }
 
     // Upah Pegawai
     // Mengambil Seluruh Data
     getUpahPegawai() {
-        return axios.get(`https://natunasandmine.com/api/upahPegawai`)
+        return axios.get(`https://server.natunasandmine.com/api/upahPegawai`)
     }
     // Menambah Data
     addUpahPegawai(value) {
-        return axios.post(`https://natunasandmine.com/api/upahPegawai`, value)
+        return axios.post(`https://server.natunasandmine.com/api/upahPegawai`, value)
     }
     // Mengubah Data
     editUpahPegawai(params, value) {
         console.log(`${params}:${value}`)
-        return axios.put(`https://natunasandmine.com/api/upahPegawai/${params}`, value)
+        return axios.put(`https://server.natunasandmine.com/api/upahPegawai/${params}`, value)
     }
     //Menghapus Data
     deleteUpahPegawai(params) {
-        return axios.delete(`https://natunasandmine.com/api/upahPegawai/${params}`)
+        return axios.delete(`https://server.natunasandmine.com/api/upahPegawai/${params}`)
     }
     // Upah Kasir
     // Mengambil Seluruh Data
     getUpahKasir() {
-        return axios.get(`https://natunasandmine.com/api/upahKasir`)
+        return axios.get(`https://server.natunasandmine.com/api/upahKasir`)
     }
     // Menambah Data
     addUpahKasir(value) {
-        return axios.post(`https://natunasandmine.com/api/upahKasir`, value)
+        return axios.post(`https://server.natunasandmine.com/api/upahKasir`, value)
     }
     // Mengubah Data
     editUpahKasir(params, value) {
         console.log(`${params}:${value}`)
-        return axios.put(`https://natunasandmine.com/api/upahKasir/${params}`, value)
+        return axios.put(`https://server.natunasandmine.com/api/upahKasir/${params}`, value)
     }
     //Menghapus Data
     deleteUpahKasir(params) {
-        return axios.delete(`https://natunasandmine.com/api/upahKasir/${params}`)
+        return axios.delete(`https://server.natunasandmine.com/api/upahKasir/${params}`)
     }
     // Harga Pasir
     // Mengambil Seluruh Data
     getHargaPasir() {
-        return axios.get(`https://natunasandmine.com/api/hargaPasir`)
+        return axios.get(`https://server.natunasandmine.com/api/hargaPasir`)
     }
     // Menambah Data
     addHargaPasir(value) {
-        return axios.post(`https://natunasandmine.com/api/hargaPasir`, value)
+        return axios.post(`https://server.natunasandmine.com/api/hargaPasir`, value)
     }
     // Mengubah Data
     editHargaPasir(params, value) {
         console.log(`${params}:${value}`)
-        return axios.put(`https://natunasandmine.com/api/hargaPasir/${params}`, value)
+        return axios.put(`https://server.natunasandmine.com/api/hargaPasir/${params}`, value)
     }
     //Menghapus Data
     deleteHargaPasir(params) {
-        return axios.delete(`https://natunasandmine.com/api/hargaPasir/${params}`)
+        return axios.delete(`https://server.natunasandmine.com/api/hargaPasir/${params}`)
     }
 
     // resoucesForm
     getResourcesForm() {
-        return axios.get(`https://natunasandmine.com/api/rf`)
+        return axios.get(`https://server.natunasandmine.com/api/rf`)
     }
 
     //pencatatan
     getPencatatan() {
-        return axios.get(`https://natunasandmine.com/api/pencatatan`)
+        return axios.get(`https://server.natunasandmine.com/api/pencatatan`)
     }
     addPencatatan(value) {
-        return axios.post(`https://natunasandmine.com/api/pencatatan`, value)
+        return axios.post(`https://server.natunasandmine.com/api/pencatatan`, value)
     }
     deletePencatatan(value) {
-        return axios.post(`https://natunasandmine.com/api/pencatatanDelete`, value)
+        return axios.post(`https://server.natunasandmine.com/api/pencatatanDelete`, value)
     }
     //BON Truk
     getBon() {
-        return axios.get(`https://natunasandmine.com/api/bon`)
+        return axios.get(`https://server.natunasandmine.com/api/bon`)
     }
 
     //Dashboard
     getDashboard() {
-        return axios.get(`https://natunasandmine.com/api/dashboard`)
+        return axios.get(`https://server.natunasandmine.com/api/dashboard`)
     }
     //new Password
     sendPassword(value) {
-        return axios.post(`https://natunasandmine.com/api/resetPassword`,value)
+        return axios.post(`https://server.natunasandmine.com/api/resetPassword`,value)
     }
 }
